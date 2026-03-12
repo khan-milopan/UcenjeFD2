acdf67dcc9299c7a930c236548118f9f4b7ea18d
khan-milopan <khan.milopan@gmail.com>
2026-03-12 20:56:58 +0100

    12.3.

---
+++ 3_REACT/EdunovaAPP/src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { Container } from 'react-bootstrap'
+import Izbornik from './components/Izbornik'
+
+function App() {
+
+  return (
+    <Container>
+      <Izbornik/>
+    </Container>
+  )
+}
+
+export default App
